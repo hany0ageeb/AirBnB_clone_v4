301acc79e6642910956fa5bf90c6984ea325a68f
hany0ageeb <hany0ageeb@gmail.com>
2024-04-04 01:52:34 -0700

    creating templates directory inside web_dynamic and move 0-hbnb.html file to it!

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,7 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__, template_folder='./')
+app = Flask(__name__, template_folder='templates')
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
