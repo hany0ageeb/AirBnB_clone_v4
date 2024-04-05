af9eee6734d189eaab1e957094fd4c5860a66be6
hany0ageeb <hany0ageeb@gmail.com>
2024-04-05 00:00:56 -0700

    Task 3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(function(){
+	const amenities_ids = [];
+	const amenities_names = [];
+	const url = 'http://0.0.0.0:5001/api/v1/status/';
+	$('.popover input[type="checkbox"]').on('change', function(event){
+		if(this.checked) {
+			amenities_ids.push($(this).attr('data-id'));
+			amenities_names.push($(this).attr('data-name'));
+		} else {
+			amenities_ids.pop($(this).attr('data-id'));
+			amenities_names.pop($(this).attr('data-name'));
+		}
+		$('.amenities>h4').html(amenities_names.join(', '));
+	});
+	$.get(url, (data, textStatus) => {
+		if (textStatus.toUpperCase() === 'OK') {
+			$('div#api_status').removeClass('unavailable');
+			$('div#api_status').addClass('available');
+		} else {
+			$('div#api_status').removeClass('available');
+			$('div#api_status').addClass('unavailable');
+		}
+	});
+});
